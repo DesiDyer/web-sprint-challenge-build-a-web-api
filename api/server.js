acacdadc0c7dba04ba08ec935f94822fec7c15cc
DiamondDyer <diamond.l.dyer@gmail.com>
2020-12-04 13:21:47 -0500

    working

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
+
 const express = require('express');
 const server = express();
+const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
+server.get('/', (req, res) => {
+    res.send('hello there')
+})
+server.use(express.json())
+server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
+
 
 module.exports = server;
